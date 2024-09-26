ed91bd4ea9eaa34c3a64b37ff0d8f8d1f485a8a0
Langel <liulan4444@gmail.com>
2024-09-26 21:55:31 +0800

    version 1.0

---
+++ go.mod
@@ -0,0 +1,24 @@
+module hidell
+
+go 1.23.1
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0
+	github.com/getlantern/systray v1.2.2
+	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e
+	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
+)
+
+require (
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,245 @@
+package main
+
+import (
+	_ "embed"
+	"github.com/fsnotify/fsnotify"
+	"github.com/getlantern/systray"
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+	"github.com/lxn/win"
+	"github.com/skratchdot/open-golang/open"
+	"os"
+	"path/filepath"
+	"strings"
+	"syscall"
+)
+
+//go:embed hidell.ico
+var logo []byte
+
+var mw *walk.MainWindow
+var systrayToolTip = "HIDE Like Linux"
+
+// 控制 watchDir goroutine 的通道
+var watcherDone chan struct{}
+
+func main() {
+	//启动快捷键监听
+	//go func() {
+	//	hkey := hotkey.New()
+	//	hkey.Register(hotkey.Ctrl, 'H', func() {
+	//		fmt.Println("Push Ctrl+H")
+	//	})
+	//}()
+
+	watcherDone = make(chan struct{})
+	go watchDir(os.Getenv("USERPROFILE"), watcherDone)
+	//运行托盘菜单
+	systray.Run(onReady, onExit)
+
+}
+
+func onReady() {
+	systray.SetIcon(logo)
+	systray.SetTitle("HIDELL")
+	systray.SetTooltip(systrayToolTip)
+
+	// 定义托盘图标的右键菜单
+	//runWindow := systray.AddMenuItem("主界面", "")
+	//systray.AddSeparator()
+	//mAdd := systray.AddMenuItem("添加目录", "")
+	//systray.AddSeparator()
+	mMonitor := systray.AddMenuItem("激活", "自动隐藏新生成的点文件/点文件夹")
+	mMonitor.Check()
+	systray.AddSeparator()
+	mHide := systray.AddMenuItem("隐藏", "隐藏已存在的点文件/点文件夹")
+	mShow := systray.AddMenuItem("显示", "显示已存在的点文件/点文件夹")
+	systray.AddSeparator()
+	mAbout := systray.AddMenuItem("关于HIDELL", "")
+	systray.AddSeparator()
+	mQuit := systray.AddMenuItem("退出", "")
+
+	userHome := os.Getenv("USERPROFILE")
+	go func() {
+		for {
+			select {
+			//case <-runWindow.ClickedCh:
+			//	go func() {
+			//		runHomeWindow()
+			//	}()
+			//case <-mAdd.ClickedCh:
+			//	// TODO: 打开文件夹选择器
+			case <-mMonitor.ClickedCh:
+				if mMonitor.Checked() {
+					mMonitor.Uncheck() // 取消选中
+					if watcherDone != nil {
+						close(watcherDone) // 发送停止信号
+						watcherDone = nil
+					}
+				} else {
+					mMonitor.Check() // 选中
+					watcherDone = make(chan struct{})
+					go watchDir(userHome, watcherDone)
+				}
+			case <-mHide.ClickedCh:
+				mHide.Check()
+				mShow.Uncheck()
+				hideDotFiles(userHome)
+			case <-mShow.ClickedCh:
+				mShow.Check()
+				mHide.Uncheck()
+				unhideDotFiles(userHome)
+			case <-mAbout.ClickedCh:
+				open.Run("https://www.github.com/llanc/hidell")
+			case <-mQuit.ClickedCh:
+				systray.Quit() //退出托盘
+				return
+			}
+		}
+	}()
+
+}
+
+func onExit() {
+	// 清理资源
+	if watcherDone != nil {
+		close(watcherDone)
+	}
+}
+
+func runHomeWindow() {
+	var err error
+	mw, err = walk.NewMainWindow()
+	if err != nil {
+		panic(err)
+	}
+
+	err = MainWindow{
+		AssignTo: &mw,
+		Title:    "HIDELL",
+		MinSize:  Size{Width: 300, Height: 400},
+		Size:     Size{Width: 300, Height: 400},
+		Layout:   VBox{},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					PushButton{Text: "激活"},
+					PushButton{Text: "隐藏"},
+					PushButton{Text: "显示"},
+					PushButton{Text: "添加目录"},
+				},
+			},
+		},
+	}.Create()
+	if err != nil {
+		panic(err)
+	}
+
+	// 移除右上角的最大化按钮
+	style := win.GetWindowLong(mw.Handle(), win.GWL_STYLE)
+	style &^= win.WS_MAXIMIZEBOX
+	win.SetWindowLong(mw.Handle(), win.GWL_STYLE, style)
+
+	// 处理最小化事件
+	mw.Closing().Attach(func(canceled *bool, reason walk.CloseReason) {
+		if reason == walk.CloseReasonUnknown {
+			*canceled = true
+			mw.Hide()
+		}
+	})
+
+	mw.Run()
+}
+
+// 隐藏目录下所有以点开头的文件或文件夹
+func hideDotFiles(dir string) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), ".") {
+			hideFile(filepath.Join(dir, file.Name()))
+		}
+	}
+}
+
+// 取消隐藏目录下所有以点开头的文件或文件夹
+func unhideDotFiles(dir string) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		if strings.HasPrefix(file.Name(), ".") {
+			unhideFile(filepath.Join(dir, file.Name()))
+		}
+	}
+}
+
+// 隐藏文件
+func hideFile(path string) {
+	ptr, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		panic(err)
+		return
+	}
+	attrs, err := syscall.GetFileAttributes(ptr)
+	if err != nil {
+		panic(err)
+		return
+	}
+	syscall.SetFileAttributes(ptr, attrs|syscall.FILE_ATTRIBUTE_HIDDEN)
+}
+
+// 取消隐藏文件
+func unhideFile(path string) {
+	ptr, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		panic(err)
+		return
+	}
+	attrs, err := syscall.GetFileAttributes(ptr)
+	if err != nil {
+		panic(err)
+		return
+	}
+	syscall.SetFileAttributes(ptr, attrs&^syscall.FILE_ATTRIBUTE_HIDDEN)
+}
+
+// 监听目录变化
+func watchDir(dir string, done chan struct{}) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		panic(err)
+		return
+	}
+	defer watcher.Close()
+
+	err = watcher.Add(dir)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	for {
+		select {
+		case <-done:
+			return // 收到停止信号时退出
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			if event.Op&fsnotify.Create == fsnotify.Create {
+				if strings.HasPrefix(filepath.Base(event.Name), ".") {
+					hideFile(event.Name)
+				}
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			panic(err)
+		}
+	}
+}
